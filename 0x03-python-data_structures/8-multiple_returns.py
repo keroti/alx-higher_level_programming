533a87f49be28494a12aba908c1701a1352b80b8
keroti <peterkeroti55@gmail.com>
2022-09-26 07:16:33 -0700

    Return a tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == '':
+    if len(sentence) == 0:
         return None
     else:
         length = len(sentence)
         first = sentence[0]
-        return (length, first)
+    return (length, first)
