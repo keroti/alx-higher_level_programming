930555a3001de964c64558f2ed177347167c23cc
keroti <peterkeroti55@gmail.com>
2022-09-28 13:44:53 -0700

    Weighted avg

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    sum = 0
+    dividesum = 0
+    for num in my_list:
+        multiply = num[0] * num[1]
+        sum += multiply
+        dividesum += num[1]
+    return sum / dividesum
